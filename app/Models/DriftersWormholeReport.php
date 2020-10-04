c4bc99c31ee9697552089b678a0a9e2f21b5fd34
RazeSoldier <razesoldier@outlook.com>
2020-10-04 22:09:36 +0800

    新增drifters_wormhole_reports表用来存储流浪洞报告

---
+++ app/Models/DriftersWormholeReport.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+/**
+ * 代表流浪洞的报告。
+ *
+ * @package App\Models
+ * @property int $id
+ * @property string $system
+ * @property string $signature_name
+ * @property string $submitter
+ * @property string $notice
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|DriftersWormholeReport newModelQuery()
+ * @method static Builder|DriftersWormholeReport newQuery()
+ * @method static Builder|DriftersWormholeReport query()
+ * @method static Builder|DriftersWormholeReport whereCreatedAt($value)
+ * @method static Builder|DriftersWormholeReport whereId($value)
+ * @method static Builder|DriftersWormholeReport whereNotice($value)
+ * @method static Builder|DriftersWormholeReport whereSignatureName($value)
+ * @method static Builder|DriftersWormholeReport whereSubmitter($value)
+ * @method static Builder|DriftersWormholeReport whereSystem($value)
+ * @method static Builder|DriftersWormholeReport whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
+class DriftersWormholeReport extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['system', 'signature_name', 'submitter', 'notice'];
+}
---
+++ database/migrations/2020_10_04_135605_create_drifters_wormhole_reports_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDriftersWormholeReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drifters_wormhole_reports', function (Blueprint $table) {
+            $table->id();
+            $table->string('system');
+            $table->string('signature_name');
+            $table->string('submitter');
+            $table->string('notice')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drifters_wormhole_reports');
+    }
+}
