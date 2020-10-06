bd55db9f98096e76321b21d930c842223d7c1432
RazeSoldier <razesoldier@outlook.com>
2020-10-06 19:22:15 +0800

    添加需要监视的星系列表和流浪洞口信息

---
+++ app/DriftersWormholeSystems.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+/**
+ * 用于提供流浪洞的相关信息。
+ * @package App
+ */
+class DriftersWormholeSystems
+{
+    /**
+     * 标记这个洞是流浪洞的洞口编号
+     */
+    public const SIGNATURE_LIST = [
+        'V928',
+        'R259',
+        'S877',
+        'B735',
+        'C414',
+    ];
+}
---
+++ app/NeedWatchSystemMap.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+/**
+ * 这个类用来提供需要监视的星系和其星域的映射。
+ * @package App
+ */
+class NeedWatchSystemMap
+{
+    private const FILE_PATH = __DIR__ . '/needwatchsystems.json';
+
+    public static function getMap(): array
+    {
+        static $cache;
+        if ($cache === null) {
+            $cache = json_decode(file_get_contents(self::FILE_PATH), true);
+        }
+        return $cache;
+    }
+
+    /**
+     * 用于前端级联选择器的选项提供器
+     * @return string 级联选择器的选项JSON
+     */
+    public static function toJson()
+    {
+        $map = self::getMap();
+        $data = [];
+        foreach ($map as $region => $systems) {
+            $children = [];
+            foreach ($systems as $system) {
+                $children[] = ['label' => $system, 'value' => $system];
+            }
+            $data[] = [
+                'label' => $region,
+                'value' => $region,
+                'children' => $children,
+            ];
+        }
+
+        return json_encode($data);
+    }
+}
